75ee286f8f5bd282bb5c3a93b65342f2ee2f003d
Hippophile <paschalakisfilip@gmail.com>
2025-08-05 18:19:03 +0300

    added delete/customise buttons TODO customise-btn full color customization

--- filip/project03/logic.js
+++ filip/project03/logic.js
@@ -7,8 +7,8 @@ const titleInput = formWrap.querySelector('[name="title"]');
 const descInput = formWrap.querySelector('[name="description"]');
 const todoZone = document.getElementById('todo')
 
-var dragItem = document.querySelector('.dragElement');
-var dropZoneSet = Array.from(document.querySelectorAll('.dropZone'))
+var dragItem = document.querySelector('.dragElement'); // adding css style to the dragged item
+var dropZoneSet = Array.from(document.querySelectorAll('.dropZone')) // creating an array of all drop zones
 
 // Show the form when the "Add Task" button is clicked
 addTask.addEventListener('click', () => {
@@ -18,11 +18,10 @@ addTask.addEventListener('click', () => {
 // and hide it when the "Cancel" button is clicked.
 cancel.addEventListener('click', () => {
   formWrap.classList.add('hidden');
-
 });
 
 save.addEventListener('click', () => {
-  // 1) διάβασε τι έγραψε ο χρήστης:
+
   const title = titleInput.value;
   const desc  = descInput.value;
 
@@ -30,12 +29,10 @@ save.addEventListener('click', () => {
     return;
   }
 
-  // 2) κάνε αυτό που θες (π.χ. createNewCard(title, desc))
-
   // Create a new task element
   const newTask = document.createElement('div');
   newTask.setAttribute('draggable', 'true');
-  newTask.classList.add('dragElement'); // σημαντικό για το styling και events
+  newTask.classList.add('dragElement'); 
   newTask.id = 'task-' + Date.now();
 
   newTask.innerHTML = `<h3>${title}</h3><p>${desc}</p>`;
@@ -44,7 +41,36 @@ save.addEventListener('click', () => {
     dragItem = newTask;
   });
 
+  // Create delete task button
+  const deleteButton = document.createElement('button');
+  deleteButton.innerHTML = `
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" height="25" width="25">
+    <path fill="#6361D9" d="M8.78842 5.03866C8.86656 4.96052 8.97254 4.91663 9.08305 4.91663H11.4164C11.5269 4.91663 11.6329 4.96052 11.711 5.03866C11.7892 5.11681 11.833 5.22279 11.833 5.33329V5.74939H8.66638V5.33329C8.66638 5.22279 8.71028 5.11681 8.78842 5.03866ZM7.16638 5.74939V5.33329C7.16638 4.82496 7.36832 4.33745 7.72776 3.978C8.08721 3.61856 8.57472 3.41663 9.08305 3.41663H11.4164C11.9247 3.41663 12.4122 3.61856 12.7717 3.978C13.1311 4.33745 13.333 4.82496 13.333 5.33329V5.74939H15.5C15.9142 5.74939 16.25 6.08518 16.25 6.49939C16.25 6.9136 15.9142 7.24939 15.5 7.24939H15.0105L14.2492 14.7095C14.2382 15.2023 14.0377 15.6726 13.6883 16.0219C13.3289 16.3814 12.8414 16.5833 12.333 16.5833H8.16638C7.65805 16.5833 7.17054 16.3814 6.81109 16.0219C6.46176 15.6726 6.2612 15.2023 6.25019 14.7095L5.48896 7.24939H5C4.58579 7.24939 4.25 6.9136 4.25 6.49939C4.25 6.08518 4.58579 5.74939 5 5.74939H6.16667H7.16638ZM7.91638 7.24996H12.583H13.5026L12.7536 14.5905C12.751 14.6158 12.7497 14.6412 12.7497 14.6666C12.7497 14.7771 12.7058 14.8831 12.6277 14.9613C12.5495 15.0394 12.4436 15.0833 12.333 15.0833H8.16638C8.05588 15.0833 7.94989 15.0394 7.87175 14.9613C7.79361 14.8831 7.74972 14.7771 7.74972 14.6666C7.74972 14.6412 7.74842 14.6158 7.74584 14.5905L6.99681 7.24996H7.91638Z" clip-rule="evenodd" fill-rule="evenodd"></path>
+  </svg>`;
+  deleteButton.classList.add('deleteButton');
+  deleteButton.addEventListener("click", () => {
+    newTask.remove();
+  });
+  newTask.appendChild(deleteButton);
+
+  // Button to change task color
+  const customButton = document.createElement('button');
+  customButton.innerHTML = `
+    <span class="bar bar1"></span>
+    <span class="bar bar2"></span>
+    <span class="bar bar1"></span>`;
+  customButton.classList.add('customButton');
+  customButton.addEventListener("click", () => {
+    const colors = ['#FF5733', '#33FF57', '#3357FF', '#F0E68C', '#FF69B4'];
+    const currentColor = newTask.style.backgroundColor;
+    const nextColor = colors[(colors.indexOf(currentColor) + 1) % colors.length];
+    newTask.style.backgroundColor = nextColor;
+  }); 
+  newTask.appendChild(customButton);
+
+
   formWrap.classList.add('hidden');
+  // Clear the input fields after saving
   titleInput.value = '';
   descInput.value  = '';
 });
